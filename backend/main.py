3cf94c7bff996f4f97601e87be70a75ee74c7cf9
Ronmy <ronmysalimey.itus@gmail.com>
2026-08-18 22:01:28 -0400

    Corregir rutas de imagenes del menu sin espacios

--- backend/main.py
+++ backend/main.py
@@ -6,34 +6,34 @@ def menu():
             "name": "Fuji Roll", 
             "price": 15.0, 
             "desc": "Rollo tempurizado con cobertura de cereal, queso crema, aguacate, cebollín y camarones.", 
-            "img": "/fuji roll.jfif" 
+            "img": "/fuji-roll.jfif" 
         },
         {
             "id": 2, 
             "name": "Saiteki Ocean", 
             "price": 18.0, 
             "desc": "Ensalada de wakame y cangrejo, salmón al grill y sashimi de atún sellado.", 
-            "img": "/Saiteki Ocean.jfif"
+            "img": "/saiteki-ocean.jfif"
         },
         {
             "id": 3, 
             "name": "Zen Roll", 
             "price": 12.0, 
             "desc": "Uramaki con zanahoria kimpa, pepino, aguacate y cebollín.", 
-            "img": "/Zen roll.jfif"
+            "img": "/zen-roll.jfif"
         },
         {
             "id": 4, 
             "name": "Sashimi de Salmón", 
             "price": 14.0, 
             "desc": "Cortes frescos de salmón premium de alta calidad.", 
-            "img": "/sashimi de salmon.jfif"
+            "img": "/sashimi-de-salmon.jfif"
         },
         {
             "id": 5, 
             "name": "Bushi Crispy Pizza", 
             "price": 16.0, 
             "desc": "Nuestra deliciosa pizza sushi crujiente con los mejores ingredientes.", 
-            "img": "/Bushi-crispy-pizza.jfif"
+            "img": "/bushi-crispy-pizza.jfif"
         }
     ]
