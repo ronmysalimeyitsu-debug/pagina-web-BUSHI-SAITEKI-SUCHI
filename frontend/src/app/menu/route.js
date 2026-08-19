20257e5e2ea95c742189358473804f3adabb876e
Ronmy <ronmysalimey.itus@gmail.com>
2026-08-18 22:16:28 -0400

    Agregar API route de menu en frontend para compatibilidad con Vercel

---
+++ frontend/src/app/menu/route.js
@@ -0,0 +1,43 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const menu = [
+    {
+        "id": 1, 
+        "name": "Fuji Roll", 
+        "price": 15.0, 
+        "desc": "Rollo tempurizado con cobertura de cereal, queso crema, aguacate, cebollín y camarones.", 
+        "img": "/fuji-roll.jfif" 
+    },
+    {
+        "id": 2, 
+        "name": "Saiteki Ocean", 
+        "price": 18.0, 
+        "desc": "Ensalada de wakame y cangrejo, salmón al grill y sashimi de atún sellado.", 
+        "img": "/saiteki-ocean.jfif"
+    },
+    {
+        "id": 3, 
+        "name": "Zen Roll", 
+        "price": 12.0, 
+        "desc": "Uramaki con zanahoria kimpa, pepino, aguacate y cebollín.", 
+        "img": "/zen-roll.jfif"
+    },
+    {
+        "id": 4, 
+        "name": "Sashimi de Salmón", 
+        "price": 14.0, 
+        "desc": "Cortes frescos de salmón premium de alta calidad.", 
+        "img": "/sashimi-de-salmon.jfif"
+    },
+    {
+        "id": 5, 
+        "name": "Bushi Crispy Pizza", 
+        "price": 16.0, 
+        "desc": "Nuestra deliciosa pizza sushi crujiente con los mejores ingredientes.", 
+        "img": "/bushi-crispy-pizza.jfif"
+    }
+  ];
+
+  return NextResponse.json(menu);
+}
